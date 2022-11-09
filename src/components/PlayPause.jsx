f95e541e09c4a9e89f019d25a9ac98ee76844174
Amy Hwang <728amyh@gmail.com>
2022-11-08 22:01:35 -0500

    added lyriks sidebar

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,7 +1,7 @@
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
 const PlayPause = ({ isPlaying, activeSong, song, handlePause, handlePlay }) => (isPlaying && activeSong?.title === song.title ? (
-  <FaPlayCircle
+  <FaPauseCircle
     size={35}
     className="text-gray-300"
     onClick={handlePause}
