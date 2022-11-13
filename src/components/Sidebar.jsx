13872724067657382d5b99d765e194ca100ee90a
Amy Hwang <728amyh@gmail.com>
2022-11-12 22:00:29 -0500

    glass morphism menu works fully functional

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { HiOutlineMenu } from 'react-icons/hi';
 import { RiCloseLine } from 'react-icons/ri';
 import { logo } from '../assets';
 import { links } from '../assets/constants';
@@ -16,7 +17,6 @@ const NavLinks = ({ handleClick }) => (
         <item.icon className="w-6 h-6 mr-2" />
         {item.name}
 
-
       </NavLink>
     ))}
   </div>
@@ -24,8 +24,7 @@ const NavLinks = ({ handleClick }) => (
 );
 
 const Sidebar = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState
-    (false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <>
@@ -33,8 +32,21 @@ const Sidebar = () => {
         <img src={logo} alt="logo" className="w-full h-14 object-contain" />
         <NavLinks />
       </div>
+      <div className="absolute md:hidden block top-6 right-3">
+        {mobileMenuOpen ? (
+          <RiCloseLine className="w-6 h-6 text-white mr-2"
+            onClick={() => setMobileMenuOpen(false)} />
+        ) : <HiOutlineMenu className="w-6 h-6 text-white mr-2"
+          onClick={() => setMobileMenuOpen(true)} />}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] 
+      backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
     </>
-  )
+  );
 
 };
 
