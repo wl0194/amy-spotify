3243153ece8b3298a87b96c609f164f439466040
Amy Hwang <728amyh@gmail.com>
2022-11-15 13:42:10 -0500

    you can swipe through artists

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -64,6 +64,30 @@ const TopPlay = () => {
                         <p className="text-gray-300 text-base cursor-pointer">See more</p>
                     </Link>
                 </div>
+                <Swiper
+                    slidesPerView="auto"
+                    spaceBetween={15}
+                    freeMode
+                    centeredSlides
+                    centeredSlidesBounds
+                    modules={[FreeMode]}
+                    className="mt-4"
+                >
+
+                    {topPlays?.map((song, i) => (
+                        <SwiperSlide
+                            key={song?.key}
+                            style={{ width: '25%', height: 'auto' }}
+                            className="shadow-lg rounded-full animate-slideright"
+                        >
+                            <Link to={`/artists/${song?.artists[0].adamid}`}>
+                                <img src={song?.images.background} alt="name"
+                                    className="rounded-full w-full object-cover" />
+                            </Link>
+                        </SwiperSlide>
+                    ))}
+
+                </Swiper>
             </div>
         </div >
     )
